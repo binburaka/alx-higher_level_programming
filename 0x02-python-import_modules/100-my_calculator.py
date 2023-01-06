31d4bfc55fb04244438899b5297f41b0b1db38d6
Binburaka <ushindiburaka@gmail.com>
2023-01-06 12:25:39 -0800

    task 2

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     if argv_count is not 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif sys.argv[2] not in operators:
+    else if sys.argv[2] not in operators:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
     else:
@@ -16,10 +16,10 @@ if __name__ == "__main__":
         b = int(sys.argv[3])
         if sys.argv[2] is "+":
             print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif sys.argv[2] is "-":
+        else if sys.argv[2] is "-":
             print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif sys.argv[2] is "*":
+        else if sys.argv[2] is "*":
             print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif sys.argv[2] is "/":
+        else if sys.argv[2] is "/":
             print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 
