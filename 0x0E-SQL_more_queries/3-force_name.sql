1a53ca0582acdbd1fb678d9a086874b0c9661a9a
binburaka <ushindiburaka@gmail.com>
2023-03-15 19:48:13 +0000

    Database Queries

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,5 +1,3 @@
-reates the table force_name.
-CREATE TABLE IF NOT EXISTS `force_name` (
-    `id`   INT,
-    `name` VARCHAR(256) NOT NULL
-);
+-- creates the table force_name on your MySQL server
+-- Holberton School
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
