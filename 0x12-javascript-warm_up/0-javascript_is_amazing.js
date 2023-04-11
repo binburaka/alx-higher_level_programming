030feb3d3c93f0ab84c4d487d874affa9e013b1f
Binburaka <ushindiburaka@gmail.com>
2023-04-11 07:21:53 -0700

    javascript

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,6 @@
+#!/usr/bin/nodw
+/*
+ * print Javascript is Amazing
+ */
+const myVar = 'Javascript is amazing;
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/*
+ * print 3 lines
+ */
+const myVar = 'C is fun\n' + 'python is cool\n' + 'Javascript is amazing;
+console.log(myVar);
