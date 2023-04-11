7695cf95494a9cb34e8d6054d377561e8bbbbb4c
Binburaka <ushindiburaka@gmail.com>
2023-04-11 07:59:17 -0700

    mandatory

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,6 @@
-#!/usr/bin/node
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+!/usr/bin/node
+/*
+  print 3 lines
+*/
+const myVar = 'C is fun\n' + 'Python is cool\n' + 'Javascript is amazing';
+console.log(myVar);
