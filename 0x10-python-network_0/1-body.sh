d25ba7fe6299770425cd34bf1c2b517e0ae3a4ca
binburaka <ushindiburaka@gmail.com>
2023-04-28 20:11:19 +0000

    network

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#This is to sends a request to thhe URL, and displays the size of the body of the response
+curl -sL "$1"
---
+++ 0x10-python-network_0/3-methods.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#To take in the URL and displays all HTTP methods that the server will accept.
+curl -sI "$1" | grep Allow | cut -d " " -f 2-
